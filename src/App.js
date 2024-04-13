7e2e4cd6a4a3eddfcc437cf78378599523f41418
Jayssen De Castro <jayssendecastro@gmail.com>
2024-04-13 17:27:21 +0100

    added hamburger menu-incomplete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './styles/style.css';
 import Navbar from './components/Navbar';
-
+import Hero from './components/Hero';
 function App() {
   return (
- 
+    <>
     <Navbar />
+    <Hero />
+
+    </>
     
     
  
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,29 +2,49 @@ import React from 'react'
 
 
 const Navbar = () => {
+
+    const hamburger = () => {
+        // const hamburgerClick = document.querySelector('.hamburger-nav');
+
+        // hamburgerClick.addEventListener('click', () => {
+        //     hamburgerClick.classList.toggle('isActive');
+        // });
+        console.log('click');
+
+    }
+
   return (
     <section className='navbar'>
-
+        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
+<link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet"></link>
+        <header className='nav-header'>
         <div className='logo'> Logo </div>
 
         {/* hamburger menu */}
         <div className='hamburger-wrapper'>
-        <div className='hamburger-nav isActive'>
-            <div className='bar-1'></div>
-            <div className='bar-2'></div>
-            <div className='bar-3'></div>
+        <div className='hamburger-nav' onClick={(e) => { e.target.classList.toggle('isActive')}}>
+            <div className='bar'></div>
+            <div className='bar'></div>
+            <div className='bar'></div>
         </div>
+       
         </div>
-
-
-{/* 
-        desktop nav */}
+        {/*       desktop nav */}
         <div className='nav-links'>
             <li><a> About me </a></li>
             <li><a> Projects </a></li>
             <li><a> Contact </a></li>
             
         </div>
+        </header>
+
+        <span className='hamburger-menu'>
+            <li><a> About me </a></li>
+            <li><a> Projects </a></li>
+            <li><a> Contact </a></li>
+
+        </span>
 
     
     </section>
